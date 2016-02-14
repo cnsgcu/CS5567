7d9655d5687109d8ab98097beeba2b524e7e7e7a
cnsgcu <csc823@gmail.com>
2016-02-14 02:31:27 -0600

    Create classifier.R

---
+++ r_hw1/classifier.R
@@ -0,0 +1,8 @@
+cals <- c(mean, var)
+match_urls <- c('https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/usopen.csv', 'https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/ausopen.csv')
+
+match_stats <- do.call(rbind, lapply(match_urls, read.csv))
+
+params <- do.call(rbind, lapply(cals, function(cal){ as.data.frame(lapply(match_stats[,1:30], cal)) }))
+
+prod(unname(sapply(params, function(p){p[2]/p[1]})))
