0d2f50a462e9e3939bfb7160ca9bc5548d576617
cnsgcu <csc823@gmail.com>
2016-02-14 02:54:58 -0600

    Update classifier.R

--- r_hw1/classifier.R
+++ r_hw1/classifier.R
@@ -1,6 +1,23 @@
-cals <- c(mean, var)
-match_urls <- c('https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/usopen.csv', 'https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/ausopen.csv')
+match_urls <- c(
+	'https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/usopen.csv',
+	'https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/ausopen.csv'
+)
 
 match_stats <- do.call(rbind, lapply(match_urls, read.csv))
 
-params <- do.call(rbind, lapply(cals, function(cal){ as.data.frame(lapply(match_stats[,1:30], cal)) }))
+team1_won_matches <- match_stats[match_stats$Match.Winner==0, 1:30]
+team2_won_matches <- match_stats[match_stats$Match.Winner==1, 1:30]
+
+training_gnb <- function(train) {
+	cals <- c(mean, var)
+	params <- do.call(rbind, lapply(cals, function(cal){ as.data.frame(lapply(train, cal)) }))
+	return(params)
+}
+
+pstprob <- function(x, params) {
+	cal_params <- rbind(params, x)
+	prob <- prod(unname(sapply(cal_params, function(p){ 1/sqrt(2 * pi * p[2]) * exp((p[3] - p[1])^2 / p[2]) } )))	
+}
+
+team1_params <- training_gnb(head(team1_won_matches, 10))
+team2_params <- training_gnb(head(team2_won_matches, 10))
