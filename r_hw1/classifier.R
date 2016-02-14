e70b9d8cb78fea0da7ca6c27a7a09553af75d91f
cnsgcu <csc823@gmail.com>
2016-02-14 02:32:45 -0600

    Update classifier.R

--- r_hw1/classifier.R
+++ r_hw1/classifier.R
@@ -4,5 +4,3 @@ match_urls <- c('https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw1/usop
 match_stats <- do.call(rbind, lapply(match_urls, read.csv))
 
 params <- do.call(rbind, lapply(cals, function(cal){ as.data.frame(lapply(match_stats[,1:30], cal)) }))
-
-prod(unname(sapply(params, function(p){p[2]/p[1]})))
