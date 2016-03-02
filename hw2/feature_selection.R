fe01292ccc8672472bc411ff710d1e771637608d
cnsgcu <csc823@gmail.com>
2016-03-01 18:38:31 -0600

    Create feature_selection.R

---
+++ hw2/feature_selection.R
@@ -0,0 +1,12 @@
+tennis_csv <- c('https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw2/split_ausopen.csv', 'https://raw.githubusercontent.com/cnsgcu/CS5567/master/hw2/split_usopen.csv')
+
+tennis_players <- do.call(rbind,lapply(tennis_csv, read.csv))
+
+won_players <- subset(tennis_players, Won == 1)
+lost_players <- subset(tennis_players, Won == 0)
+
+pvalues <- lapply(1:15, function(ci) {t.test(won_players[,ci], lost_players[,ci])$p.value})
+
+pvaluesBonferroni = p.adjust(pvalues, method = "bonferroni", n = length(pvalues))
+
+colnames(tennis_players)[which(pvaluesBonferroni < 0.05)]
